cebd23491fb8fe44315d47ecea39d9584c70c78a
t-itagaki <t-itagaki@atware.co.jp>
2024-10-10 11:51:52 +0900

    Adjust style

--- src/components/QuizCard.tsx
+++ src/components/QuizCard.tsx
@@ -1,6 +1,6 @@
 import {JSX} from "react";
 import {CardProps, Quiz} from "../types";
-import {Box, List, ListItem, Typography} from "@mui/material";
+import {Box, List, ListItem, Paper, Typography} from "@mui/material";
 
 type QuizCardProps = CardProps<Quiz>
 
@@ -8,18 +8,23 @@ function QuizCard(props: QuizCardProps): JSX.Element {
     const {data: quiz} = props;
 
     return (
-        <Box sx={{ mt: 5 }}>
-            <Typography variant="h1" component="h1" fontWeight="bold" color="warning">{quiz.question}</Typography>
-            <List>
-                {quiz.choices.map((choice, idx) => (
-                    <ListItem key={idx}>
-                        <Typography variant="h2" component={"h2"} fontWeight="bold">
-                            {idx + 1} : {choice}
-                        </Typography>
-                    </ListItem>
-                ))}
-            </List>
-        </Box>
+        <Paper elevation={6} sx={{mt: 5}}>
+            <Box sx={{p: 3, bgcolor: 'secondary.dark'}}>
+                <Typography variant="h1" component="h1" fontWeight="bold" color="common.white">{quiz.question}</Typography>
+            </Box>
+            <Box sx={{p: 3}}>
+                <List>
+                    {quiz.choices.map((choice, idx) => (
+                        <ListItem key={idx}>
+                            <Typography variant="h2" component={"h2"} fontWeight="bold">
+                                {idx + 1} : {choice}
+                            </Typography>
+                        </ListItem>
+                    ))}
+                </List>
+            </Box>
+        </Paper>
     );
 }
+
 export default QuizCard;
